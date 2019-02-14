917c5acdf66bb01ee9a8f04f6eb90591f54d1600
ÂTramtadama <kakadustew@seznam.cz>
2019-02-14 18:40:11 +0100

    added with no_grad to evalutaion

--- evaluations/cnn.py
+++ evaluations/cnn.py
@@ -30,21 +30,22 @@ from torch.autograd import Variable
 
 def extract_cnn_feature(model, inputs, pool_feature=False):
     model.eval()
-    inputs = to_torch(inputs)
-    inputs = Variable(inputs, volatile=True).cuda()
-    if pool_feature is False:
-        outputs = model(inputs)
-        return outputs
-    else:
-        # Register forward hook for each module
-        outputs = {}
+    with torch.no_grad():
+        inputs = to_torch(inputs)
+        inputs = Variable(inputs).cuda()
+        if pool_feature is False:
+            outputs = model(inputs)
+            return outputs
+        else:
+            # Register forward hook for each module
+            outputs = {}
 
 
-        def func(m, i, o): outputs['pool_feature'] = o.data.view(n, -1)
-        hook = model.module._modules.get('features').register_forward_hook(func)
-        model(inputs)
-        hook.remove()
-        # print(outputs['pool_feature'].shape)
-        return outputs['pool_feature']
+            def func(m, i, o): outputs['pool_feature'] = o.data.view(n, -1)
+            hook = model.module._modules.get('features').register_forward_hook(func)
+            model(inputs)
+            hook.remove()
+            # print(outputs['pool_feature'].shape)
+            return outputs['pool_feature']
 
     
