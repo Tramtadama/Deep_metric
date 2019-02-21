84a286401c4918f91e025576c5f54e990534fa16
ÂTramtadama <kakadustew@seznam.cz>
2019-02-21 22:47:16 +0100

    deb

--- losses/triplet_custom.py
+++ losses/triplet_custom.py
@@ -1,4 +1,5 @@
 from torch import nn
+import pdb
 
 class TripletLoss(nn.Module):
     def __init__(self, margin=0.1, **kwargs):
@@ -8,6 +9,7 @@ class TripletLoss(nn.Module):
     def forward(self, inputs, targets, sim_mat, idx, t2i, all_idx_l):
 
         for i in range(targets.shape[0]):
+            pdb.set_trace()
             sample = sim_mat[idx[i], :]
             target_iden = str(targets[i].item())
             pos_ind_l = t2i[target_iden]
