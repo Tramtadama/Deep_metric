1655f876ad1babf9b75b5f3845ab94e11f34dc5b
ÂTramtadama <kakadustew@seznam.cz>
2019-02-24 18:37:42 +0100

    aa

--- evaluations/get_apn.py
+++ evaluations/get_apn.py
@@ -10,17 +10,18 @@ def get_apn(inputs, targets, features, idx, t2i, all_idx_l):
     sim_mat = torch.mm(inputs, features.t().cuda()) 
 
     for i in range(targets.shape[0]):
-        anchor[i] = features[i]
+        ind = idx[i]
+        anchor[i] = features[ind]
         
         sample = sim_mat[i, :]
         target_iden = str(targets[i].item())
         pos_ind_l = copy.deepcopy(t2i[target_iden])
         neg_ind_l = list(set(all_idx_l) - set(pos_ind_l))
-        pos_ind_l.remove(idx[i])
-        pos, pos_idx = torch.min(sample[pos_ind_l], dim=0)
-        neg, neg_idx = torch.max(sample[neg_ind_l], dim=0)
-        
-        pdb.set_trace()
+        pos_ind_l.remove(ind)
+        _, pos_idx = torch.min(sample[pos_ind_l], dim=0)
+        pos = pos_ind_l[pos_idx]
+        _, neg_idx = torch.max(sample[neg_ind_l], dim=0)
+        neg = neg_ind_l[neg_idx]
         pos[i] = features[pos_idx]
         neg[i] = features[neg_idx]
 
